691d716e8769d2c4c3c61b194e49112577d12d71
dangwd <dangwd.vicidev@gmail.com>
2025-04-01 09:44:52 +0700

    update

--- src/views/client/pages/ProductDetail.vue
+++ src/views/client/pages/ProductDetail.vue
@@ -82,8 +82,8 @@
                     <Avatar crossorigin="anonymous" :image="User?.thumbnail" class="mr-2 object-cover" size="large" shape="circle" />
                     <div class="flex flex-col gap-2 w-full">
                         <strong>{{ User?.name }}</strong>
-                        <Rating v-model="ratingValue.rating" />
-                        <Textarea v-model="ratingValue.content" auto-resize class="w-full"></Textarea>
+                        <Rating v-model="cmtPayload.rating" />
+                        <Textarea v-model="cmtPayload.content" auto-resize class="w-full"></Textarea>
                         <div class="flex justify-end">
                             <Button @click="confirmSubmit()" label="Gửi"></Button>
                         </div>
@@ -93,12 +93,12 @@
             <div v-for="(item, index) in detail.comments" :key="index" class="flex flex-col gap-3">
                 <div class="flex gap-2 p-2">
                     <Avatar crossorigin="anonymous" :image="item.user?.thumbnail" class="mr-2 object-cover" size="large" shape="circle" />
-                    <div class="flex flex-col gap-1">
+                    <div class="flex flex-col gap-2">
                         <strong>{{ item.user?.name }}</strong>
                         <Rating v-model="item.rating" />
+                        <span>{{ item.content }}</span>
                     </div>
                 </div>
-                <span>{{ item.content }}</span>
                 <hr />
             </div>
         </div>
@@ -126,10 +126,11 @@ const router = useRouter();
 const route = useRoute();
 const quantity = ref(1);
 const value = ref(4);
-const ratingValue = ref({
+const cmtPayload = ref({
     content: '',
     rating: 0
 });
+const clearCmtPayload = JSON.stringify(cmtPayload.value);
 const User = ref();
 const cartStore = useCartStore();
 const Products = ref([]);
@@ -180,10 +181,11 @@ const fetchAllProducts = async () => {
 };
 const confirmSubmit = async () => {
     try {
-        const res = await API.create(`product/${detail.value._id}/comment`, ratingValue.value);
+        const res = await API.create(`product/${detail.value._id}/comment`, cmtPayload.value);
         if (res.data) {
             fetchDetailProduct();
             proxy.$notify('S', 'Thành công!', toast);
+            cmtPayload.value = JSON.parse(clearCmtPayload);
         }
     } catch (error) {
         console.log(error);
@@ -193,7 +195,9 @@ const getMe = async () => {
     try {
         const res = await API.get(`get-me`);
         User.value = res.data.metadata;
-    } catch (error) {}
+    } catch (error) {
+        console.log(error);
+    }
 };
 </script>
 <style></style>
